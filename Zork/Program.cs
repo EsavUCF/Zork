cf3aafb20493a6db4c3fd93ce6a5abe45b66e5cf
EsavUCF <70274517+EsavUCF@users.noreply.github.com>
2021-09-15 11:32:23 -0400

    Zork. 2.2 with 2.3 revisions.
    Zork. 2.2 & 2.3 revisions.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,158 +1,190 @@
-﻿//Note, the notes in my pushes are for me, as I'm still learning. If I should not have these. Please let me know ASAP. 
-using System;
+﻿using System;
 using System.Collections.Generic;
+
 
 namespace Zork
 {
-    class Program
-    {
 
-        private static string Location //private static string Location => Rooms[LocationColumn];
+    internal class Program
+    {
+        private static string CurrentRoom
         {
             get
             {
-                return Rooms[LocationRow,LocationColumn];          //Properties are known as attributes. It describes the class. For example a pizza class would have the toppings in it, size etc.
-                 
+                return Rooms[Location.Row, Location.Column];
             }
-
 
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-
-            //Commands command = Commands.UNKNOWN;
-
-            string outputString=""; 
-           
-            while (true)
-                
-                 
-
+            Commands command = Commands.UNKNOWN;
+            while (command !=Commands.QUIT)
             {
+                Console.WriteLine(CurrentRoom);
+                Console.Write(">");
+                command = ToCommand(Console.ReadLine().Trim());
 
-                Console.Write($"{Location} \n");  //\N make a new line rather than a < 
-                Commands command = ToCommand(Console.ReadLine().Trim());
-                if (command == Commands.QUIT)//you have loops in loops. 
+
+                switch (command)
                 {
-                    break;
-                }
-
-               // Console.WriteLine(CurrentRoom);
-               // Console.Write(">");
-               // command = ToCommand(Console.ReadLine().Trim());
-                
-                switch (command) //This segment takes your commands from commands.cs and puts a reponse which is dictated below.
-
-                   
-                
-                
-                
-                
-                {
+                    case Commands.QUIT:
+                        Console.WriteLine("Thank you for playing!");
+                        break;
 
                     case Commands.LOOK:
-                    outputString = "This is an open field west of a white house, with a boarded front door. A Rubber mat saying 'Welcome to Zork! lies by the door.";
-                    break;
-
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("A Rubber mat saying 'Welcome to Zork!' lies by the door");
                         break;
+
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
-                    case Commands.WEST:
-                        outputString = Move(command) ? $"You moved {command}." :"The way is shut!";
-
-
-
-
-                   //   if (Move(command))
-                   //   {
-                   //       outputString = $"You moved {command}.";
-                    //  }
-                    //  else
-                   //   {
-                   //       outputString = "The way is shut!";
-                   //   }
-
-                       break;
+                    case Commands.WEST: 
+                        if (Move(command) == false)
+                        {
+                            Console.WriteLine("The way is shut!");
+                        }
+                        break;
 
                     default:
-                        outputString = "Unknown command";
+                        Console.WriteLine("Unknown Command.");
                         break;
+
 
 
                 }
 
-
-            Console.WriteLine(outputString);
             }
-            Console.WriteLine("Finished");
-
-
         }
-        private static Commands ToCommand(string commandString)
-        {
-           
-            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;            // <-- This is an Expression Bodied Method  //
-        }
-        
-        
-        private static string[,] Rooms =
-        {
-            {"Rocky Trail", "South of House","Canyon View"},
-            {"Forest","West of House","Behind House" },       
-            {"Dense Woods", "North of House", "Clearing" }
 
-        };                 //initiallizer list  (Makes an array of 5 rooms)         ZORK 2.1
-
-      //  private static readonly List<Commands> Directions = new List<Commands>
-      //  { Commands.NORTH,
-       //   Commands.SOUTH,
-        //  Commands.EAST,
-        //  Commands.WEST
-        //};
-        //private static (int Row, int Column) Location = (1, 1);
-
-        private static int LocationColumn = 1; //ints are used to access the array. Aka forest would be assigned to a int     Rooms and Locationcolumn are known as fields
-        private static int LocationRow = 1;
         private static bool Move(Commands command)
         {
-            bool didMove = false;
-
+            Assert.IsTrue(IsDirection(command), "invalid direction");
+            
+            bool isValidMove = true;
             switch (command)
             {
-                case Commands.NORTH when LocationRow < Rooms.GetLength(0) - 1:
-                    LocationRow++;
-                    didMove = true;
+                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
                     break;
 
-                case Commands.SOUTH when LocationRow > 0:
-                    LocationRow--;
-                    didMove = true;  //whens can only be in switches.
+                case Commands.SOUTH when Location.Row > 0:
+                    Location.Row--;
                     break;
 
-                    
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column++;
+                    break;
 
-                case Commands.EAST when LocationColumn < Rooms.GetLength(1) - 1:
-                    LocationColumn++;
-                    didMove = true;
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
                     break;
-               
-                case Commands.WEST when LocationColumn > 0:
-                 LocationColumn--;
-                  didMove = true;  //whens can only be in switches.
+
+                default:
+                    isValidMove = false;
                     break;
-               
-                
+
+
+
+
+
+
+
+
             }
-            
-            return didMove;
+
+            return isValidMove;
+
+
         }
+        private static Commands ToCommand(string commandString) =>
+            Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
+        private static bool IsDirection(Commands command) => Directions.Contains(command);
+
+        private static readonly string[,] Rooms =
+        {
+            {"Rocky Trail", "South of House", "Canyon View" },
+            {"Forest", "West of House", "Behind House" },
+            {"Dense Woods", "North of House", "Clearing" }
+        };
+
+        private static readonly List<Commands> Directions = new List<Commands>
+        {   Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
+        private static (int Row, int Column) Location = (1, 1);
+
+
+
+
     }
-}//
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
