a01f7740853a685bcee4b55be4fbf1731a2ef16c
EsavUCF <70274517+EsavUCF@users.noreply.github.com>
2021-09-18 12:36:37 -0400

    Zork 3.1
    Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,7 +7,7 @@ namespace Zork
 
     internal class Program
     {
-        private static string CurrentRoom
+        public static Room CurrentRoom
         {
             get
             {
@@ -18,7 +18,7 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-
+            InitializeRoomDescriptions(); //This calls for the descriptions which are below. 
             Commands command = Commands.UNKNOWN;
             while (command !=Commands.QUIT)
             {
@@ -34,7 +34,7 @@ namespace Zork
                         break;
 
                     case Commands.LOOK:
-                        Console.WriteLine("A Rubber mat saying 'Welcome to Zork!' lies by the door");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
 
@@ -104,11 +104,11 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static readonly string[,] Rooms =
+        private static readonly Room[,] Rooms =
         {
-            {"Rocky Trail", "South of House", "Canyon View" },
-            {"Forest", "West of House", "Behind House" },
-            {"Dense Woods", "North of House", "Clearing" }
+            {new Room("Rocky Trail"), new Room ("South of House"), new Room ("Canyon View") },        //3.1 Stuff
+            {new Room ("Forest"), new Room ("West of House"), new Room ("Behind House") },
+            {new Room("Dense Woods"), new Room ("North of House"), new Room ("Clearing") }
         };
 
         private static readonly List<Commands> Directions = new List<Commands>
@@ -123,66 +123,32 @@ namespace Zork
 
 
 
+
+
+        private static void InitializeRoomDescriptions()   //3.1 stuff
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail."; //Rocky Trail
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred. "; //South of House
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall"; //Canyon View
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you"; //Forest
+            Rooms[1, 1].Description = "This is a open field west of a white house, with a boarded front door."; //west of house
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window that is slightly ajar"; //behind house
+
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east there appears to be sunlight"; //Dense Woods
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here and all windows are barred"; //North of house
+            Rooms[2, 2].Description = "You are in a clearing with a forest surrounding you on the west and south";                        //Clearing
+
+        }
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 
---
+++ Zork/Room.cs
@@ -0,0 +1,23 @@
+﻿namespace Zork
+{
+    public class Room
+    {
+
+        public string Name { get; }
+
+        public string Description { get; set; }
+
+
+       
+        public Room(string name, string description = "")
+
+        {
+            Name = name;
+            Description = Description;
+
+        }
+
+        public override string ToString() => Name;
+    }
+
+}
