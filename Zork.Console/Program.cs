8d1e90b875aa02ab69eb14857c4463bf3ab3eb54
EsavUCF <70274517+EsavUCF@users.noreply.github.com>
2021-11-08 15:54:46 -0500

    Zork in Unity Sample Code Implemented.

--- Zork.Console/Program.cs
+++ Zork.Console/Program.cs
@@ -1,25 +1,22 @@
-﻿using System;
+﻿using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
-
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             const string defaultGameFilename = "Zork.json";
             string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
 
-            Game game = Game.Load(gameFilename);
-            Console.WriteLine("Welcome to Zork!");
+            Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(gameFilename));
             game.Run();
-            Console.WriteLine("Thank you for playing!");
         }
 
         private enum CommandLineArguments
         {
             GameFilename = 0
-
         }
     }
 }
